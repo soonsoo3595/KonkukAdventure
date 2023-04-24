88ec87c2a0d51cf2ec6f8c213c650f1b1c221907
durnfish <117414891+durnfish@users.noreply.github.com>
2023-04-24 15:21:10 +0900

    Player Data setting Complete

--- Assets/Scripts/DataMgr.cs
+++ Assets/Scripts/DataMgr.cs
@@ -3,6 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
+#region 플레이어 데이터
+[System.Serializable]
+public class PlayerData
+{
+    public int UserID;
+    public int grade;
+    public int semester;
+    public int creditLimit;
+    public int creditReserve;
+    public int scoreReserve;
+    public int KUPointReserve;
+}
+
+[System.Serializable]
+public class PlayerDataList
+{
+    public List<PlayerData> data;
+}
+#endregion
+
 #region 건물 데이터
 [System.Serializable]
 public class BuildingData
@@ -55,34 +75,35 @@ public class LectureDataList
 }
 #endregion
 
-
 public static class DataMgr
 {
     // json 파일 경로
+    
+    //플레이어 Json 경로
+    private static string playerJsonPath = "Assets/JSON/PlayerData.json";
+
+    #region 학과, 건물 Json 경로
     private static string buildingJsonPath = "Assets/JSON/BuildingData.json";
     private static string departmentJsonPath = "Assets/JSON/DepartmentData.json";
     private static string lectureJsonPath = "Assets/JSON/LectureData.json";
-
-    private static List<int> buildingRecord;
-    private static List<LectureData> lectureRecord;
+    #endregion
 
     public static BuildingDataList Buildings { get; private set; }
     public static DepartmentDataList Departments { get; private set; }
     public static LectureDataList Lectures { get; private set; }
 
-    public static List<int> BuildingRecord = new List<int>();
-    public static List<LectureData> LectureRecord = new List<LectureData>();
+    public static PlayerDataList player { get; private set; }
 
     public static void LoadData()
     {
+        string playerJson = File.ReadAllText(playerJsonPath);
         string buildingJson = File.ReadAllText(buildingJsonPath);
         string departmentJson = File.ReadAllText(departmentJsonPath);
         string lectureJson = File.ReadAllText(lectureJsonPath);
 
-
+        player = JsonUtility.FromJson<PlayerDataList>(playerJson);
         Buildings = JsonUtility.FromJson<BuildingDataList>(buildingJson);
         Departments = JsonUtility.FromJson<DepartmentDataList>(departmentJson);
         Lectures = JsonUtility.FromJson<LectureDataList>(lectureJson);
-
     }
 }
