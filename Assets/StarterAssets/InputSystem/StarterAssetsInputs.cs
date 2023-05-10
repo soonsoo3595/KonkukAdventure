32fc80245280d8cb626d71ad1c1d1c57ca11aa84
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2023-05-10 16:24:51 +0900

    Input 수정 및 불필요한 씬 삭제
    마우스로 인게임에서 카메라 조정 중 ESC 키 누르면 UI 클릭하고 다시 ESC 눌러 게임 내로 돌아갈 수 있음

--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -1,80 +1,86 @@
+using System;
 using UnityEngine;
 #if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
 #endif
 
-namespace StarterAssets
+public class StarterAssetsInputs : MonoBehaviour
 {
-	public class StarterAssetsInputs : MonoBehaviour
-	{
-		[Header("Character Input Values")]
-		public Vector2 move;
-		public Vector2 look;
-		public bool jump;
-		public bool sprint;
+	[Header("Character Input Values")]
+	public Vector2 move;
+	public Vector2 look;
+	public bool jump;
+	public bool sprint;
 
-		[Header("Movement Settings")]
-		public bool analogMovement;
+	[Header("Movement Settings")]
+	public bool analogMovement;
 
-		[Header("Mouse Cursor Settings")]
-		public bool cursorLocked = true;
-		public bool cursorInputForLook = true;
+	[Header("Mouse Cursor Settings")]
+	public bool cursorLocked = true;
+	public bool cursorInputForLook = true;
 
 #if ENABLE_INPUT_SYSTEM
-		public void OnMove(InputValue value)
-		{
-			MoveInput(value.Get<Vector2>());
-		}
+	public void OnMove(InputValue value)
+	{
+		MoveInput(value.Get<Vector2>());
+	}
 
-		public void OnLook(InputValue value)
+	public void OnLook(InputValue value)
+	{
+		if (cursorInputForLook)
 		{
-			if(cursorInputForLook)
-			{
-				LookInput(value.Get<Vector2>());
-			}
+			LookInput(value.Get<Vector2>());
 		}
+	}
 
-		public void OnJump(InputValue value)
-		{
-			JumpInput(value.isPressed);
-		}
+	public void OnJump(InputValue value)
+	{
+		JumpInput(value.isPressed);
+	}
 
-		public void OnSprint(InputValue value)
-		{
-			SprintInput(value.isPressed);
-		}
+	public void OnSprint(InputValue value)
+	{
+		SprintInput(value.isPressed);
+	}
+
+	public void OnEscape(InputValue value)
+	{
+		cursorLocked = !cursorLocked;
+        cursorInputForLook = !cursorInputForLook;
+
+        SetCursorState(cursorLocked);
+    }
 #endif
 
 
-		public void MoveInput(Vector2 newMoveDirection)
-		{
-			move = newMoveDirection;
-		} 
-
-		public void LookInput(Vector2 newLookDirection)
-		{
-			look = newLookDirection;
-		}
-
-		public void JumpInput(bool newJumpState)
-		{
-			jump = newJumpState;
-		}
-
-		public void SprintInput(bool newSprintState)
-		{
-			sprint = newSprintState;
-		}
-
-		private void OnApplicationFocus(bool hasFocus)
-		{
-			SetCursorState(cursorLocked);
-		}
-
-		private void SetCursorState(bool newState)
-		{
-			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
-		}
+	public void MoveInput(Vector2 newMoveDirection)
+	{
+		move = newMoveDirection;
 	}
-	
+
+	public void LookInput(Vector2 newLookDirection)
+	{
+		look = newLookDirection;
+	}
+
+	public void JumpInput(bool newJumpState)
+	{
+		jump = newJumpState;
+	}
+
+	public void SprintInput(bool newSprintState)
+	{
+		sprint = newSprintState;
+	}
+
+	private void OnApplicationFocus(bool hasFocus)
+	{
+		SetCursorState(cursorLocked);
+	}
+
+	public void SetCursorState(bool newState)
+	{
+		Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+	}
 }
+	
