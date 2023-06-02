beec33f473a3a2d146c78fe209d4b868b175c7a9
durnfish <tkgkdgksek12@gmail.com>
2023-06-03 00:20:53 +0900

    마지 충돌 해결

--- Assets/UGUI/Script/Lecture/StudyButton.cs
+++ Assets/UGUI/Script/Lecture/StudyButton.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class TakeLectureInfo : MonoBehaviour
+public class StudyButton : MonoBehaviour
 {
     LectureData lectureData;
-    [SerializeField] private Text nameSpace;
+    [SerializeField] private TMP_Text InfoSpace;
     List<int> buildingRecord;
 
     public Popup semesterOverPopup;
@@ -14,14 +15,12 @@ public class TakeLectureInfo : MonoBehaviour
 
     void Start()
     {
-        this.gameObject.SetActive(false);
-        LectureDataSet.LectureStudy += Active;
+        LectureCell.SetLecture += Active;
         buildingRecord = DataMgr.BuildingRecord;
     }
 
     void Active(LectureData lectureData)
     {
-        this.gameObject.SetActive(true);
         SetLectureInfo(lectureData);
         SetLectureName(lectureData.name);
     }
@@ -33,7 +32,7 @@ public class TakeLectureInfo : MonoBehaviour
 
     void SetLectureName(string name)
     {
-        nameSpace.text = name;
+        InfoSpace.text = name;
     }
 
      public void ClickStudyButton()
@@ -48,15 +47,18 @@ public class TakeLectureInfo : MonoBehaviour
         }
         else
         {
+            //학점 반영
+            DataMgr.record.totalCredit += lectureData.course_credit;
             DataMgr.player.creditReserve = ReflectionData();
-            Debug.Log(DataMgr.player.creditReserve + " 학점");
+            //Ku포인트 반영
+            DataMgr.record.totalKupoint += lectureData.KU_point;
+            DataMgr.player.KUPointReserve += lectureData.KU_point;
+
             GameManager.instance.renewalPopup();
         }
     }
 
     // (현재 학점에 선택한 강의의 학점이 더해진것)을 리턴하는 메서드
-    // 현재 이 코드는 학점 제한으로 인해 선택한 과목을 수강 할 수 없음에도 기록을 하고있다.
-    // 따라서 수정 필요
     private int ReflectionData()
     {
         int buildingNum = Portal.portal.BuildNum;
@@ -73,5 +75,11 @@ public class TakeLectureInfo : MonoBehaviour
 
         DataMgr.LectureRecord.Add(lectureData);
         return DataMgr.player.creditReserve + lectureData.course_credit;
+    }
+
+    //닫으면 설명창 초기화
+    private void OnDisable()
+    {
+        InfoSpace.text = "";
     }
 }
